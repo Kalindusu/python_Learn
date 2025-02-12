0b54586a3e2901d0b0783ccb10a5163d2df3f888
Kalindu Sudaraka <146382563+Kalindusu@users.noreply.github.com>
2025-02-12 23:10:13 +0530

    Ternory operators

--- meaning.py
+++ meaning.py
@@ -1,7 +1,11 @@
 meaning = 8
 print('')
 
-if meaning > 10:
-    print('The meaning of life is True', meaning)
-else:
-    print('The meaning of life is false', meaning)
+# if meaning > 10:
+#     print('The meaning of life is True', meaning)
+# else:
+#     print('The meaning of life is false', meaning)
+
+#Ternery operators
+
+print('Right on ')if meaning > 10 else print('Wrong on')
