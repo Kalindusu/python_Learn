e799c7eaab2cd9e6aac4e7c87aefa90f248d6ec4
Kalindu Sudaraka <146382563+Kalindusu@users.noreply.github.com>
2025-02-13 22:05:00 +0530

    list update

--- Lesson 2/lists.py
+++ Lesson 2/lists.py
@@ -1,1 +1,52 @@
 users=['Kalindu','Kasun','Kamal','Kasuni']
+
+data=['kalindu',42,True]
+
+emptylist=[]
+
+print("Dave" in emptylist)
+
+print(users[0])
+print(users[-2])
+
+print(users.index('Kasun'))
+
+print(users[0:2])
+print(users[1:])
+
+
+print(len(data))
+
+users.append('Kasuni')
+print(users)
+
+users+=['Nimal']
+print(users)
+
+users.extend(['Sunil','Kamal'])
+print(users)
+
+# users.extend(data)
+# print(users)
+
+users.insert(2,'Bob')
+print(users)
+
+users[3:4]=['Anna','sarawan','sudaraka','saman']
+print(users)
+
+users[1:3]=['Robe','jpa']
+print(users)
+
+users.remove('Kalindu')
+print(users)
+
+users.pop()
+print(users)
+
+del users[0]
+print(users)
+
+#del data
+data.clear()
+print(data)
