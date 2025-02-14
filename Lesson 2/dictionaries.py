95f799cb3a572c96b5164984f0c46ed424138339
Kalindu Sudaraka <146382563+Kalindusu@users.noreply.github.com>
2025-02-14 10:14:50 +0530

    dictionaries

---
+++ Lesson 2/dictionaries.py
@@ -0,0 +1,26 @@
+#dictionary
+
+band={
+    "vocal": "vocalist",
+    "guitar": "guitarist",
+    "drums": "drummer"
+}
+
+band2=dict(vocal="vocalist", guitar="guitarist", drums="drummer")
+
+print(band)
+print(band2)
+
+print(type(band))
+print(len(band))
+print(type(band2))
+
+#Access items
+print(band["vocal"])
+print(band.get("name"))
+
+#list all keys
+print(band.keys())
+
+#list all values
+print(band.values())
