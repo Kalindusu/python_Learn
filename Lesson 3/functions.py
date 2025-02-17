ebd8bb890ce20b1d02d287f4f66f943cf95ee581
Kalindu Sudaraka <146382563+Kalindusu@users.noreply.github.com>
2025-02-17 10:01:57 +0530

    function learn

--- Lesson 3/functions.py
+++ Lesson 3/functions.py
@@ -1,4 +1,21 @@
-def hello():
-    print("Hello, World!")
+# def hello():
+#     print("Hello, World!")
 
-hello()
+# hello()
+# def add_numbers(num1, num2):
+#     print(num1 + num2)
+
+# add_numbers(2, 3)  # Output: 5
+
+
+# def multiple_items(*args):
+#     print(args)
+    
+
+# multiple_items(1, 2, 3, 4, 5)  # Output: (1, 2, 3, 4, 5)
+
+def mult_named_items(**kwargs):
+    print(kwargs)
+    print(type(kwargs))
+
+mult_named_items(first=1, second=2, third=3)  # Output: {'first': 1, 'second': 2, 'third': 3}
